0c23f9788144665bb7ea1cd8fd5be2e0c0f53404
Mikael Gramont <mgramont@gmail.com>
2020-06-03 12:00:29 +0200

    Initial commit: - created 3 pages at /, /login and /profile - added a folder in /api to respond to API requests via a PHP script

---
+++ api/index.php
@@ -0,0 +1,6 @@
+/*
+ * This is a super basic implementation of an API.
+ * It's written in PHP to emphasize that it is separate from the Next.js server.
+ */
+
+// TODO: handle requests to /api/login
